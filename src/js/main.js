c0c4131115823a586c531975a19d0d5f90ed26ed
Fabricio <101414179+FaArMa@users.noreply.github.com>
2022-09-03 11:38:42 -0300

    Added the necessary functions for Max Packets

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ window.onload = function() {
 };
 
 let validfps = [];
+let validpkt = [];
 
 function get_validfps() {
     // Stores all possible valid values in an array (no duplicates)
@@ -26,6 +27,9 @@ function com_maxfps() {
             fpsValue.className = "text-success";
         else
             fpsValue.className = "text-warning";
+        // If the FPS value is changed the Max Packets value must be changed
+        document.getElementById("pkt-range").value = "0";
+        cl_maxpackets();
     };
     fpsRange.fpsValue();
 }
@@ -35,4 +39,37 @@ function com_maxFrameTime(fps) {
     const ft = 1000 / fps;
     const frametime = document.getElementById("frame-time");
     frametime.innerHTML = (fps != 0) ? "Frametime: " + ft.toFixed(2) + " ms" : "Unlimited FPS";
+}
+
+function cl_maxpackets() {
+    const fpsValue = +document.getElementById("fps-val").innerHTML;
+    const pktRange = document.getElementById("pkt-range");
+    const pktValue = document.getElementById("pkt-val");
+    const pktNotice = document.getElementById("pkt-notice");
+    let pkt;
+    validpkt = [fpsValue];
+    let i = 2;
+    // Stores all possible valid values in an array (no duplicates)
+    while (validpkt[validpkt.length - 1] > 1)
+        if (validpkt[validpkt.length - 1] != (pkt = ~~(fpsValue / i++) + 1))
+            validpkt.push(pkt);
+    // Set the max input attribute to the array size
+    pktRange.pktValue = function() {
+        pktValue.innerHTML = validpkt[this.value];
+        pktRange.max = validpkt.length - 1;
+    };
+    pktRange.pktValue();
+    // Change class and text accordingly
+    if (+pktValue.innerHTML > 100 || +pktValue.innerHTML < 15) {
+        pktValue.className = "text-warning";
+        pktNotice.innerHTML = "Possibly out of range";
+    } else {
+        pktValue.className = "text-success";
+        pktNotice.innerHTML = "";
+    }
+    if (fpsValue == 0) {
+        pktValue.className = "text-danger";
+        pktValue.innerHTML = "Unknown for Unlimited FPS";
+        pktNotice.innerHTML = "";
+    }
 }
